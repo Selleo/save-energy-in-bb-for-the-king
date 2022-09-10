92c7d25a26d1bda6adbc764cbced9b2701442a23
Szymon Kieloch <skieloch@gmail.com>
2022-09-10 23:29:27 +0200

    chore: add data types for locations

---
+++ pages/api/location.type.ts
@@ -0,0 +1,77 @@
+export class Address {
+  street: string = ''
+  number: string = ''
+  city: string = ''
+
+  // coordinates
+  long: string = ''
+  lat: string = ''
+}
+
+export enum InstallationType {
+  Wind = 'Wind',
+  SolarPanels = 'SolarPanels',
+}
+
+export enum PlacementType {
+  Ground = 'Ground',
+  Roof = 'Roof',
+}
+
+export class Installation {
+  type: InstallationType = InstallationType.SolarPanels
+  production: number = 0 // production in kWh
+  createdAt: Date = new Date()
+  pacement: PlacementType = PlacementType.Ground
+}
+
+export enum BuildingType {
+  House = 'House',
+  Garage = 'Garage'
+}
+
+export enum RoofMaterialType {
+  Papa = 'Papa',
+  Dachowka = 'Dachowka',
+  Blacha = 'Blacha'
+}
+
+export class Building {
+  roofArea: number = 0 // m2
+  roofMaterial: RoofMaterialType = RoofMaterialType.Blacha
+  type: BuildingType = BuildingType.House
+}
+
+export class Person {
+  name: string = ''
+  phone: string = ''
+  email: string = ''
+}
+
+export class ConsumptionDataPoint {
+  value: number = 0
+  date: Date = new Date()
+}
+
+export class ProductionDataPoint {
+  value: number = 0
+  date: Date = new Date()
+}
+
+export class Location {
+  address: Address = new Address()
+  installations: Installation[] = []
+  buildings: Building[] = []
+  primaryContact?: Person // whom to contact about the building
+  peopleNumber?: number
+
+  estimatedConsumption?: number // consumption in kWh
+
+  productionDataPoints: ProductionDataPoint[] = []
+  consumptionDataPoints: ConsumptionDataPoint[] = []
+}
+
+
+
+
+
--- pages/api/locations.ts
+++ pages/api/locations.ts
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Data = {
-  name: string
-}
+import { Location } from './location.type'
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Location[]>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(
+    [
+      new Location(),
+      new Location(),
+      new Location(),
+    ]
+  )
 }
