bb8c7ee675d1438e7a873f54f766d5a47751c701
Dawid Loranc <dawidloranc@gmail.com>
2022-09-11 02:58:35 +0200

    Add Details component

--- components/LocationsPanel/LocationsPanel.tsx
+++ components/LocationsPanel/LocationsPanel.tsx
@@ -1,52 +1,44 @@
+import { useState } from 'react'
 import { Location } from '../../pages/api/location.type'
-import { Buildings } from './Buildings'
-import { Contact } from './Contact'
-import { Installations } from './Installations'
+import { Details } from './Details'
+
 import styles from './LocationsPanel.module.scss'
-import { Statistics } from './Statistics'
 
 interface LocationPanelProps {
   data: Location[],
 }
 
 export function LocationsPanel({ data }: LocationPanelProps) {
+  const [currentIndex, setCurrentIndex] = useState<number|null>(null)
+
   return (
     <div className={styles.locationsPanel}>
-      <h1>Miejsca</h1>
+      {currentIndex === null && (
+          <>
+            <h1>Miejsca</h1>
 
-      <div className={styles.locations}>
-        {data.map((location: Location) => (
-          <div className={styles.location}>
-            <div className={styles.address}>
-              {location.address.street}{' '}  {location.address.number}<br/>
-              {location.address.city}<br/>
-
-              {location.address.lat}<br/>
-              {location.address.long}<br/>
+            <div className={styles.locations}>
+              {data.map((location: Location, index) => (
+                <div className={styles.location} onClick={() => setCurrentIndex(index)}>
+                  <div className={styles.address}>
+                    {location.address.street}{' '}  {location.address.number}<br />
+                    {location.address.city}<br />
+                    {location.address.lat}<br />
+                    {location.address.long}<br />
+                  </div>
+                  {location.peopleNumber && (
+                    <div className={styles.peopleNumber}>
+                      {location.peopleNumber} osób
+                    </div>
+                  )}
+                </div>
+              ))}
             </div>
+          </>
+        )
+      }
 
-            {location.peopleNumber && (
-              <div className={styles.peopleNumber}>
-                {location.peopleNumber} osób
-              </div>
-            )}
-
-            <Installations installations={location.installations}/>
-
-            <Buildings buildings={location.buildings}/>
-
-            <Contact primaryContact={location.primaryContact} />
-
-            <Statistics
-              estimatedDailyConsumption={location.estimatedDailyConsumption}
-              estimatesYearlyConsumption={location.estimatesYearlyConsumption}
-              estimatesYearlyProduction={location.estimatesYearlyProduction}
-              productionDataPoints={location.productionDataPoints}
-              consumptionDataPoints={location.consumptionDataPoints}
-            />
-          </div>
-        ))}
-      </div>
+      {currentIndex !== null && <Details location={data[currentIndex]} />}
     </div>
   )
 }
