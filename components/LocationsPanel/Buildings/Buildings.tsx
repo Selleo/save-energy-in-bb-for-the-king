1f3a0ff825b4c046553eeb6b53b52358a9ef57e7
Dawid Loranc <dawidloranc@gmail.com>
2022-09-11 16:01:32 +0200

    Add icons

--- components/LocationsPanel/Buildings/Buildings.tsx
+++ components/LocationsPanel/Buildings/Buildings.tsx
@@ -1,5 +1,6 @@
-import { Building, BuildingType } from "../../../pages/api/location.type";
-import styles from "./Buildings.module.scss";
+import { Building, BuildingType } from '../../../pages/api/location.type'
+import { HouseIcon } from '../../Icon/HouseIcon'
+import styles from './Buildings.module.scss'
 
 interface BuildingsProps {
   buildings: Building[];
@@ -17,20 +18,24 @@ export function Buildings({ buildings }: BuildingsProps) {
     }
   };
 
-  return buildings.length > 0 ? (
-    <div className={styles.buildings}>
-      {buildings.map((building: Building, index) => (
-        <div className={styles.building} key={index}>
-          Powierzchnia dachu: {building.roofArea} m<sup>2</sup>
-          <br />
-          Materiał: {building.roofMaterial}
-          <br />
-          Typ budynku: {getType(building.type)}
-          <br />
+  return (
+    <>
+      <HouseIcon/>
+      {buildings.length > 0 ? (
+        <div className={styles.buildings}>
+          {buildings.map((building: Building, index) => (
+            <div className={styles.building} key={index}>
+              Powierzchnia dachu: {building.roofArea} m<sup>2</sup><br />
+              Materiał: {building.roofMaterial}<br />
+              Typ budynku: {getType(building.type)}<br />
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
-  ) : (
-    <div>Brak budynków</div>
-  );
+      )
+        :
+        (
+          <div>Brak budynków</div>
+        )}
+    </>
+  )
 }
